43f73ef3a0c62cf3c6c6fc098d4009f8336ad8fd
KTural <tural.kazimli00@mail.ru>
2020-09-04 14:13:47 +0200

    Update Account

--- src/main/java/com/example/Account.java
+++ src/main/java/com/example/Account.java
@@ -19,73 +19,74 @@ public class Account extends AbstractBehavior<Account.Command> {
 
         final long paymentOrderId;
         final long date;
-        final boolean instructAccount;
-        final long currency;
-        final long accountId;
+        final Double amount;
+        final String accountId;
         final long bankId;
-        final ActorRef<CheckAccountBalance> amount;
-        final ActorRef<CheckAccountBalance> checkPaymentOrderId;
+        final ActorRef<PaymentOrderStatus> checkPaymentOrderId;
 
-        public SubmitPaymentOrder(final long paymentOrderId, final long date, final boolean instructAccount,
-                    final long accountId, final long bankId,
-                    final long currency, final ActorRef<CheckAccountBalance> amount, 
-                    final ActorRef<CheckAccountBalance> checkPaymentOrderId) {
+        public SubmitPaymentOrder(final long paymentOrderId, final long date,
+                    final Double amount, final String accountId,
+                    final long bankId,
+                    final ActorRef<PaymentOrderStatus> checkPaymentOrderId) {
 
                         this.paymentOrderId = paymentOrderId;
                         this.date = date;
-                        this.instructAccount = instructAccount;
+                        this.amount = amount;
                         this.accountId = accountId;
                         this.bankId = bankId;
-                        this.currency = currency;
-                        this.amount = amount;
                         this.checkPaymentOrderId = checkPaymentOrderId;
 
                     }
     }
 
+    public static final class PaymentOrderStatus {
+        
+        final long paymentOrderId;
+        final String status;
+
+        public PaymentOrderStatus(final long paymentOrderId, final String status) {
+
+                        this.paymentOrderId = paymentOrderId;
+                        this.status = status;
+
+        }
+    }
+
     public static final class CheckAccountBalance implements Command {
 
-        final long paymentOrderId;
-        final long balance;
-        final Optional<Double> value;
-        final ActorRef<PaymentOrderVerified> verify;
-        final ActorRef<PaymentOrderRejected> reject;
+        final SubmitPaymentOrder paymentOrder;
+        protected ActorRef<PaymentOrderVerified> verify;
+        protected ActorRef<PaymentOrderRejected> reject;
 
-        public CheckAccountBalance(final long paymentOrderId, final long balance, 
-                    final Optional<Double> value, final ActorRef<PaymentOrderVerified> verify,
-                    final ActorRef<PaymentOrderRejected> reject) {
-                        
-                        this.paymentOrderId = paymentOrderId;
-                        this.balance = balance;
-                        this.value = value;
-                        this.verify = verify;
-                        this.reject = reject;
+        public CheckAccountBalance(final SubmitPaymentOrder paymentOrder) {
 
-                    }
+                        this.paymentOrder = paymentOrder;
+
+        }
     }
 
     public static final class PaymentOrderVerified {
         
         final long paymentOrderId;
-        final boolean paymentOrderCheck;
-        final long paymentOrderRequests;
-        final long numberOfATMFeeWithdrawals;
-        final long numberOfATMFeeDeposits;
-        final Optional<Double> amount;
-        final Optional<Double> balance;
+        final String transferStatus;
+        final long numberOfPaymentOrderRequests;
+        final Double amount;
+        final Double accountBalance;
+        final String orderStatus;
         
-        public PaymentOrderVerified(final long paymentOrderId, final boolean paymentOrderCheck,
-                    final long paymentOrderRequests, final long numberOfATMFeeWithdrawals,
-                    final long numberOfATMFeeDeposits,
-                    final Optional<Double> amount, final Optional<Double> balance) {
+        public PaymentOrderVerified(final long paymentOrderId,
+                    final String transferStatus,
+                    final long numberOfPaymentOrderRequests, 
+                    final Double amount,
+                    final Double accountBalance,
+                    final String orderStatus) {
 
                         this.paymentOrderId = paymentOrderId;
-                        this.paymentOrderCheck = paymentOrderCheck;
-                        this.paymentOrderRequests = paymentOrderRequests;
-                        this.numberOfATMFeeWithdrawals = numberOfATMFeeWithdrawals;
-                        this.numberOfATMFeeDeposits = numberOfATMFeeDeposits;
+                        this.transferStatus = transferStatus;
+                        this.numberOfPaymentOrderRequests = numberOfPaymentOrderRequests;
                         this.amount = amount;
-                        this.balance = balance;
+                        this.accountBalance = accountBalance;
+                        this.orderStatus = orderStatus;
 
                     }
     }
@@ -93,17 +94,18 @@ public class Account extends AbstractBehavior<Account.Command> {
     public static final class PaymentOrderRejected {
 
         final long paymentOrderId;
-        final boolean paymentOrderCheck;
-        final Optional<Double> amount;
-        final Optional<Double> balance;
+        final Double amount;
+        final Double balance;
+        final String orderStatus;
 
-        public PaymentOrderRejected(final long paymentOrderId, final boolean paymentOrderCheck,
-                    final Optional<Double> amount, final Optional<Double> balance) {
+        public PaymentOrderRejected(final long paymentOrderId,
+                    final Double amount, final Double balance,
+                    final String orderStatus) {
 
                         this.paymentOrderId = paymentOrderId;
-                        this.paymentOrderCheck = paymentOrderCheck;
                         this.amount = amount;
                         this.balance = balance;
+                        this.orderStatus = orderStatus;
 
                     }        
     }
@@ -146,21 +148,21 @@ public class Account extends AbstractBehavior<Account.Command> {
     public static final class InstructOppositeAccount implements Command {
 
         final long paymentOrderId;
-        final boolean instructAccount;
+        final boolean accountInstruction;
         final long currency;
         final long balance;
         final ActorRef<Payment.IdentifyRouteToOppositeAccount> amount;
         final ActorRef<Payment.IdentifyRouteToOppositeAccount> receiveBankId;
         final ActorRef<Payment.IdentifyRouteToOppositeAccount> receiveAccountId;
 
-        public InstructOppositeAccount(final long paymentOrderId, final boolean instructAccount, 
+        public InstructOppositeAccount(final long paymentOrderId, final boolean accountInstruction, 
                     final long currency, final long balance, 
                     final ActorRef<Payment.IdentifyRouteToOppositeAccount> amount, 
                     final ActorRef<Payment.IdentifyRouteToOppositeAccount> receiveBankId,
                     final ActorRef<Payment.IdentifyRouteToOppositeAccount> receiveAccountId) {
 
                         this.paymentOrderId = paymentOrderId;
-                        this.instructAccount = instructAccount;
+                        this.accountInstruction = accountInstruction;
                         this.currency = currency;
                         this.balance = balance;
                         this.amount = amount;
@@ -173,11 +175,11 @@ public class Account extends AbstractBehavior<Account.Command> {
     public static final class CompletePaymentOrder implements Command {
 
         final long paymentOrderId;
-        final long accountId;
+        final String accountId;
         final long amount;
         final ActorRef<PaymentOrderProcessed> confirmation;
 
-        public CompletePaymentOrder(final long paymentOrderId, final long accountId, 
+        public CompletePaymentOrder(final long paymentOrderId, final String accountId, 
                     final long amount, final ActorRef<PaymentOrderProcessed> confirmation) {
 
                         this.paymentOrderId = paymentOrderId;
@@ -210,19 +212,128 @@ public class Account extends AbstractBehavior<Account.Command> {
                     }
     }
 
+    static enum Passivate implements Command {
+        INSTANCE
+    }
 
-    private final long accountPaymentOrderId;
+    public static Behavior<Command> create(String accountId, Double accountBalance, Double amount) {
 
-    private final long accountAmount;
+        return Behaviors.setup(context -> new Account(context, accountId, accountBalance, amount));
 
-    private Account(final ActorContext<Command> context, 
-                    final long accountPaymentOrderId, final long accountAmount) {
+    }
+
+    private final String accountId;
+    private final Double accountBalance;
+    private final String currency;
+
+    private final String transferStatus;
+    protected long numberOfPaymentOrderRequests;
+    protected long numberOfATMFeeWithdrawals;
+    protected long numberOfATMFeeDeposits;
+
+    private Account(final ActorContext<Command> context, final String accountId, final Double accountBalance,
+            final Double amount) {
 
         super(context);
-        this.accountPaymentOrderId = accountPaymentOrderId;
-        this.accountAmount = accountAmount;
+        this.accountId = accountId;
+        this.accountBalance = accountBalance;
+        this.currency = "CZK";
 
-        context.getLog().info("Account actor entered with id {} and amount {} ", 
-                                accountPaymentOrderId, accountAmount);
+        transferStatus = "Withdraw";
+        numberOfPaymentOrderRequests = 0;
+        numberOfATMFeeWithdrawals = 0;
+        numberOfATMFeeDeposits = 0;
+
+        context.getLog().info("Account actor is created with :: id {}, balance {}, currency {} ", accountId, accountBalance, currency);
+
     }
+
+    @Override
+    public Receive<Command> createReceive() {
+
+        return newReceiveBuilder()
+            .onMessage(SubmitPaymentOrder.class, this::onSubmitPaymentOrder)
+            .onMessage(CheckAccountBalance.class, this::onCheckAccountBalance)
+            .onMessage(DebitCurrentAccount.class, this::onDebitCurrentAccount)
+            .onMessage(InstructOppositeAccount.class, this::onInstructOppositeAccount)
+            .onMessage(CompletePaymentOrder.class, this::onCompletePaymentOrder)
+            .onMessage(Passivate.class, m -> Behaviors.stopped())
+            .onSignal(PostStop.class, signal -> onPostStop())
+            .build();
+
+    }
+
+    private Behavior<Command> onSubmitPaymentOrder(final SubmitPaymentOrder paymentOrder) {
+        getContext().getLog().info("Submit Payment Order id={} command received with Account id={} and Bank id={} on {} ", 
+            paymentOrder.paymentOrderId, paymentOrder.accountId, paymentOrder.bankId, paymentOrder.date);
+
+        paymentOrder.checkPaymentOrderId.tell(new PaymentOrderStatus(paymentOrder.paymentOrderId, "SUBMITTED"));
+
+        this.getContext().getSelf().tell(new CheckAccountBalance(paymentOrder));
+        
+        return this;
+    }
+
+    private Behavior<Command> onCheckAccountBalance(final CheckAccountBalance checkBalance) {
+
+        getContext().getLog().info("Checking balance for Payment Order Id = {} ", checkBalance.paymentOrder.paymentOrderId);
+        if (checkBalance.paymentOrder.amount <= this.accountBalance) {
+
+            if (transferStatus == "Withdraw") {
+
+                numberOfATMFeeWithdrawals += 1;                
+
+            } else if (transferStatus == "Deposit") {
+
+                numberOfATMFeeDeposits += 1;
+
+            }
+
+            numberOfPaymentOrderRequests += 1;
+
+
+            checkBalance.verify.tell(new PaymentOrderVerified(checkBalance.paymentOrder.paymentOrderId, 
+            transferStatus, numberOfPaymentOrderRequests, checkBalance.paymentOrder.amount, this.accountBalance, "VERIFIED"));
+
+            getContext().getLog().info("Balance for Payment Order Id = {}, status = {} ", checkBalance.paymentOrder.paymentOrderId,
+            "VERIFIED");
+
+        } else {
+
+            checkBalance.reject.tell(new PaymentOrderRejected(checkBalance.paymentOrder.paymentOrderId, 
+            checkBalance.paymentOrder.amount, this.accountBalance, "REJECTED"));
+
+            getContext().getLog().info("Balance for Payment Order Id = {}, status = {} ", checkBalance.paymentOrder.paymentOrderId,
+            "REJECTED");
+
+        }
+
+
+        return this;
+
+    }
+    
+    private Behavior<Command> onDebitCurrentAccount(final DebitCurrentAccount debitAccount) {
+
+        return this;
+
+    }
+    
+    private Behavior<Command> onInstructOppositeAccount(final InstructOppositeAccount instructAccount) {
+
+        return this;
+
+    }
+    
+    private Behavior<Command> onCompletePaymentOrder(final CompletePaymentOrder completeOrder) {
+
+        return this;
+
+    }
+    
+    private Behavior<Command> onPostStop() {
+        getContext().getLog().info("Account actor is stopped with :: id {}, balance {}, currency {} ", accountId, accountBalance, currency);
+        return Behaviors.stopped();
+    }    
+    
 }
