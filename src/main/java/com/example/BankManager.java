bbab69e2e0057cb6523fc79364081cec44804cb1
KTural <tural.kazimli00@mail.ru>
2020-09-04 14:14:21 +0200

    Update BankManager

--- src/main/java/com/example/BankManager.java
+++ src/main/java/com/example/BankManager.java
@@ -1,11 +1,25 @@
 package com.example;
 
-import java.util.Optional;
+// # reactive-account
 
-import akka.actor.typed.ActorRef;
-import akka.actor.typed.Behavior;
-import akka.actor.typed.PostStop;
-import akka.actor.typed.javadsl.AbstractBehavior;
-import akka.actor.typed.javadsl.ActorContext;
-import akka.actor.typed.javadsl.Behaviors;
-import akka.actor.typed.javadsl.Receive;
+import akka.actor.typed.ActorSystem;
+
+public class BankManager {
+
+    private static String accountId;
+    private static Double accountBalance;
+    private static Double amount;
+
+    public static void main(String[] args) {
+        // Given account id of user
+        accountId = "ACCOUNT NUMBER: 09898211873";
+        accountBalance = 32768.029;
+        // Amount that user wants to send to another account (as payment order)
+        amount = 10000.90;
+
+        // Create Account actor
+        ActorSystem.create(Account.create(accountId, accountBalance, amount), "Account-actor");
+    }
+
+}
+
