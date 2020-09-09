e7c19eeb3d6422368bac05c6a1753d712bd58eab
KTural <tural.kazimli00@mail.ru>
2020-09-09 16:06:01 +0200

    Update behavior and receive for commands and event

--- src/main/java/com/example/Payment.java
+++ src/main/java/com/example/Payment.java
@@ -1,9 +1,5 @@
 package com.example;
 
-import java.util.Optional;
-
-import com.example.Account.*;
-
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.PostStop;
@@ -55,5 +51,84 @@ public class Payment extends AbstractBehavior<Account.Command> {
 
 
     }
-    
+
+    static enum Passivate implements Account.Command {
+
+        INSTANCE
+
+    }
+
+    public static Behavior<Account.Command> create(String accountId, String bankId,
+                        boolean externalAccountInstructed, boolean externalAccountCredited,
+                        boolean paymentNetworkConnected) {
+
+                            return Behaviors.setup(context -> new Payment(context, accountId, bankId, externalAccountInstructed,
+                            externalAccountCredited, paymentNetworkConnected));
+
+    }
+
+    private final String accountId;
+    private final String bankId;
+    private final boolean externalAccountInstructed;
+    private final boolean externalAccountCredited;
+    private final boolean paymentNetworkConnected;
+
+
+    private Payment(final ActorContext<Account.Command> context, final String accountId, final String bankId, 
+                        final boolean externalAccountInstructed, final boolean externalAccountCredited,
+                        final boolean paymentNetworkConnected) {
+
+                            super(context);
+                            this.accountId = accountId;
+                            this.bankId = bankId;
+                            this.externalAccountInstructed = externalAccountInstructed;
+                            this.externalAccountCredited = externalAccountCredited;
+                            this.paymentNetworkConnected = paymentNetworkConnected;
+
+                            context.getLog().info(" Payment actor is created with :: Account Id - %s, Bank Id - %s ", accountId, bankId);
+
+                            context.getLog().info(" Payment Network is connected - STATUS: %b ", paymentNetworkConnected);
+
+                            context.getLog().info(" Account Domain instructed Payment Domain. External Account Instructed - STATUS: %b ",
+                            externalAccountInstructed);
+
+
+    }
+
+    @Override
+    public Receive<Account.Command> createReceive() {
+
+        return newReceiveBuilder().onMessage(CreditInternalAccount.class, this::onCreditInternalAccount)
+                        .onMessage(CreditExternalAccount.class, this::onCreditExternalAccount)
+                        .onMessage(Passivate.class, m -> Behaviors.stopped()).onSignal(PostStop.class, signal -> onPostStop())
+                        .build();
+
+    }
+
+    private Behavior<Account.Command> onCreditInternalAccount(final CreditInternalAccount creditIntAccount) {
+
+                        getContext().getLog().info(" External Account Instruction is SUCCESSFULL! - STATUS: %b ", externalAccountInstructed);
+
+                        return this;
+
+    }
+
+    private Behavior<Account.Command> onCreditExternalAccount(final CreditExternalAccount creditIntAccount) {
+
+                        return this;
+
+    }    
+
+    private Behavior<Account.Command> onPostStop() {
+
+                        getContext().getLog().info("Payment actor is stopped with :: Account Id - %s, Bank Id - %s ", accountId, bankId);
+
+                        getContext().getLog().info(" Payment Network is disconnected - STATUS: %b ", paymentNetworkConnected);
+
+                        getContext().getLog().info(" Crediting External Account failed - STATUS: %b ", externalAccountCredited);
+                        
+                        return Behaviors.stopped();
+
+    }
+
 }
