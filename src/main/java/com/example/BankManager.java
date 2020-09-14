edd15f860b68e04d0f4a6634570111ae5f0dcb4d
KTural <tural.kazimli00@mail.ru>
2020-09-14 15:03:47 +0200

    Minor fix

--- src/main/java/com/example/BankManager.java
+++ src/main/java/com/example/BankManager.java
@@ -9,6 +9,7 @@ public class BankManager {
     private static String accountId;
     private static Double accountBalance;
     private static Double amount;
+    private static String mainCommand;
 
     public static void main(String[] args) {
         // Given account id of user
@@ -16,9 +17,10 @@ public class BankManager {
         accountBalance = 32768.029;
         // Amount that user wants to send to another account (as payment order)
         amount = 10000.90;
-
+        // One of the 3 commands : withdraw, deposit, payment in order to execute other actors and behaviors
+        mainCommand = "payment";
         // Create Account actor
-        ActorSystem.create(Account.create(accountId, accountBalance, amount), "Account-actor");
+        ActorSystem.create(Account.create(accountId, accountBalance, amount, mainCommand), "Account-actor");
     }
 
 }
