7a7f6a2535de4d5fe31662693f24736ad597f195
KTural <tural.kazimli00@mail.ru>
2020-09-15 11:48:03 +0200

    Expand parameters for starting account actor

--- src/main/java/com/example/BankManager.java
+++ src/main/java/com/example/BankManager.java
@@ -9,7 +9,12 @@ public class BankManager {
     private static String accountId;
     private static Double accountBalance;
     private static Double amount;
+
     private static String mainCommand;
+    private static String userPackage;
+
+    private static Long paymentOrderId;
+    private static long bankId;
 
     public static void main(String[] args) {
         // Given account id of user
@@ -18,9 +23,16 @@ public class BankManager {
         // Amount that user wants to send to another account (as payment order)
         amount = 10000.90;
         // One of the 3 commands : withdraw, deposit, payment in order to execute other actors and behaviors
-        mainCommand = "payment";
+        mainCommand = "Payment";
+        // Package is one of `Normal` or `Student` has different product definition features
+        userPackage = "Student";
+        // Payment order id for requesting sending amount to external account
+        paymentOrderId = 892126099L;
+        // The id of bank that external account is registered
+        bankId = 1209309204930L;
         // Create Account actor
-        ActorSystem.create(Account.create(accountId, accountBalance, amount, mainCommand), "Account-actor");
+        ActorSystem.create(Account.create(accountId, accountBalance, amount, mainCommand, userPackage, paymentOrderId, bankId),
+        "Account-actor");
     }
 
 }
