eeb6010a118054fdbe5f12b00d226d5fa3483ae0
KTural <tural.kazimli00@mail.ru>
2020-09-16 11:46:43 +0200

    Withdraw and deposit Ids update

--- src/main/java/com/example/BankManager.java
+++ src/main/java/com/example/BankManager.java
@@ -21,9 +21,12 @@ public class BankManager {
     private static boolean externalAccountCredited;
     private static boolean paymentNetworkConnected;
 
+    // For Billing domain
+    private static String withdrawalId;
+    private static String depositId;
     public static void main(String[] args) {
         // Given account id of user
-        accountId = "ACCOUNT NUMBER: 09898211873";
+        accountId = "ACCOUNT NUMBER: 0989821187";
         accountBalance = 32768.029;
         // Amount that user wants to send to another account (as payment order)
         amount = 10000.905;
@@ -34,15 +37,20 @@ public class BankManager {
         // Payment order id for requesting sending amount to external account
         paymentOrderId = 892126099L;
         // The id of bank that external account is registered
-        bankId = "BANK ID: 1209309204930CZ";
+        bankId = "BANK ID: 1209309204930125CZ";
         // Status of instruction for internal, external account and network connection in Payment domain
         internalAccountInstructed = true;
         externalAccountCredited = true;
         paymentNetworkConnected = false;
+        // Withdraw and Deposit Process Ids
+        withdrawalId = "WITHDRAWAL ID: 89298420";
+        depositId = "DEPOSIT ID: 04932409";
+
         // Create Account actor and other actors according to type of command user enters 
         if (mainCommand == "Payment") {
 
-                ActorSystem.create(Account.create(accountId, accountBalance, amount, mainCommand, userPackage, paymentOrderId, bankId),
+                ActorSystem.create(Account.create(accountId, accountBalance, amount, mainCommand, userPackage, paymentOrderId, bankId, 
+                withdrawalId, depositId),
                 "Account-actor");
                 ActorSystem.create(Payment.create(accountId, bankId, internalAccountInstructed, 
                 externalAccountCredited, paymentNetworkConnected), "Payment-actor");
@@ -50,9 +58,10 @@ public class BankManager {
 
         } else if ((mainCommand == "Withdraw") || (mainCommand == "Deposit")) {
 
-            ActorSystem.create(Account.create(accountId, accountBalance, amount, mainCommand, userPackage, paymentOrderId, bankId),
-            "Account-actor");
-            ActorSystem.create(Billing.create(), "Billing-actor");                
+                ActorSystem.create(Account.create(accountId, accountBalance, amount, mainCommand, userPackage, paymentOrderId, bankId, 
+                withdrawalId, depositId),
+                "Account-actor");
+                ActorSystem.create(Billing.create(), "Billing-actor");                
 
         }
     }
