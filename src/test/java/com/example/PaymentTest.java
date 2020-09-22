c3a4c7f6b034f474887426d2e5f5bbd68c8e3243
KTural <tural.kazimli00@mail.ru>
2020-09-22 19:40:07 +0200

    Create test for identifying route to external account

--- src/test/java/com/example/PaymentTest.java
+++ src/test/java/com/example/PaymentTest.java
@@ -1,1 +1,46 @@
 package com.example;
+
+import akka.actor.testkit.typed.javadsl.TestKitJunitResource;
+import akka.actor.testkit.typed.javadsl.TestProbe;
+import akka.actor.typed.ActorRef;
+
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+
+import static org.junit.Assert.assertEquals;
+
+import com.example.Account.*;
+import com.example.Payment.*;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class PaymentTest {
+
+   // For Payment domain
+   private final boolean internalAccountInstructed = true;
+   private final boolean externalAccountCredited = true;
+   private final boolean paymentNetworkConnected = true;    
+
+   private ActorRef<Command> paymentActor = testKit.spawn(Payment.create(AccountTest.accountId, AccountTest.bankId,
+   internalAccountInstructed, externalAccountCredited, paymentNetworkConnected));
+
+    @ClassRule
+    public static final TestKitJunitResource testKit = new TestKitJunitResource();
+
+    @Test
+    public void fTestIdentifyRouteToExternalAccount() {
+
+        TestProbe<RouteIdentified> probe = testKit.createTestProbe(RouteIdentified.class);
+
+        paymentActor.tell(new IdentifyRouteToExternalAccount(AccountTest.accountId,
+        AccountTest.paymentOrderId, AccountTest.externalAcountId, AccountTest.amount, AccountTest.bankId, 
+        "EXTERNAL ACCOUNT IS IN THE SYSTEM!", probe.getRef()));
+
+        RouteIdentified route = probe.receiveMessage();
+
+        assertEquals("EXTERNAL ACCOUNT IS IN THE SYSTEM!", route.replyTo);
+
+    }
+
+}
