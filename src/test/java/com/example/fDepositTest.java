b532e7af116398dd3b75c9af62d2720413ba9180
KTural <tural.kazimli00@mail.ru>
2020-09-26 14:23:08 +0200

    Create test for crediting deposited account

--- src/test/java/com/example/fDepositTest.java
+++ src/test/java/com/example/fDepositTest.java
@@ -12,6 +12,7 @@ import org.junit.runners.MethodSorters;
 import static org.junit.Assert.assertEquals;
 
 import com.example.Account.*;
+import com.example.Billing.*;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class fDepositTest {
@@ -22,6 +23,10 @@ public class fDepositTest {
     private ActorRef<Command> accountActor = testKit.spawn(Account.create(aAccountTest.accountId, aAccountTest.externalAcountId, 
     aAccountTest.accountBalance, aAccountTest.amount, aAccountTest.mainCommand, aAccountTest.userPackage, aAccountTest.paymentOrderId,
     aAccountTest.bankId, aAccountTest.currency, aAccountTest.withdrawalId, aAccountTest.depositId));
+
+    private ActorRef<Command> billingActor = testKit.spawn(Billing.create(aAccountTest.accountId, aAccountTest.externalAcountId, aAccountTest.bankId,
+    aAccountTest.currency, aAccountTest.amount, aAccountTest.accountBalance, aAccountTest.userPackage,
+    aAccountTest.paymentOrderId, aAccountTest.withdrawalId, aAccountTest.depositId));
 
     @Test 
     public void mTestSubmitDepositOrder() {
@@ -35,6 +40,21 @@ public class fDepositTest {
         DepositVerified deposit = probe.receiveMessage();
 
         assertEquals("VERIFIED!", deposit.replyTo);
+
+    }
+
+    @Test
+    public void nTestCreditDepositedAccount() {
+
+        TestProbe<DepositCompleted> probe = testKit.createTestProbe(DepositCompleted.class);
+
+        billingActor.tell(new CreditDepositedAccount(aAccountTest.depositId, aAccountTest.accountBalance,
+        aAccountTest.userPackage, aAccountTest.accountId, aAccountTest.date, aAccountTest.amount, aAccountTest.currency,
+        "DEPOSITED!", probe.getRef()));
+
+        DepositCompleted depositCompletion = probe.receiveMessage();
+
+        assertEquals("DEPOSITED!", depositCompletion.replyTo);
 
     }
 
