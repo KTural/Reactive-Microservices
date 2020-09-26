21345bd306c22361ff70e870cebf26c9bf3a9331
KTural <tural.kazimli00@mail.ru>
2020-09-26 14:21:23 +0200

    Create test for withdrawing account

--- src/test/java/com/example/eWithdrawalTest.java
+++ src/test/java/com/example/eWithdrawalTest.java
@@ -12,6 +12,7 @@ import org.junit.runners.MethodSorters;
 import static org.junit.Assert.assertEquals;
 
 import com.example.Account.*;
+import com.example.Billing.*;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class eWithdrawalTest {
@@ -22,6 +23,10 @@ public class eWithdrawalTest {
     private ActorRef<Command> accountActor = testKit.spawn(Account.create(aAccountTest.accountId, aAccountTest.externalAcountId, 
     aAccountTest.accountBalance, aAccountTest.amount, aAccountTest.mainCommand, aAccountTest.userPackage, aAccountTest.paymentOrderId,
     aAccountTest.bankId, aAccountTest.currency, aAccountTest.withdrawalId, aAccountTest.depositId));
+
+    private ActorRef<Command> billingActor = testKit.spawn(Billing.create(aAccountTest.accountId, aAccountTest.externalAcountId, aAccountTest.bankId,
+    aAccountTest.currency, aAccountTest.amount, aAccountTest.accountBalance, aAccountTest.userPackage,
+    aAccountTest.paymentOrderId, aAccountTest.withdrawalId, aAccountTest.depositId));
 
     @Test 
     public void kTestSubmitWithdrawal() {
@@ -48,6 +53,35 @@ public class eWithdrawalTest {
             WithdrawalRejected withdrawalReject = probe_two.receiveMessage();
 
             assertEquals("REJECTED!", withdrawalReject.replyTo);
+
+        }
+
+    }
+
+    @Test
+    public void lTestDebitWithdrawnAccount() {
+
+        TestProbe<WithdrawalCompleted> probe = testKit.createTestProbe(WithdrawalCompleted.class);
+
+        if (aAccountTest.amount < aAccountTest.accountBalance) {
+
+            billingActor.tell(new DebitWithdrawnAccount(aAccountTest.withdrawalId, aAccountTest.accountBalance,
+            aAccountTest.userPackage, aAccountTest.accountId, aAccountTest.date, aAccountTest.amount, aAccountTest.currency,
+            "WITHDRAWN!", probe.getRef()));
+
+            WithdrawalCompleted withdrawal = probe.receiveMessage();
+
+            assertEquals("WITHDRAWN!", withdrawal.replyTo);
+
+        } else {
+
+            billingActor.tell(new DebitWithdrawnAccount(aAccountTest.withdrawalId, aAccountTest.accountBalance,
+            aAccountTest.userPackage, aAccountTest.accountId, aAccountTest.date, aAccountTest.amount, aAccountTest.currency,
+            "FAILED!", probe.getRef()));
+
+            WithdrawalCompleted withdrawal = probe.receiveMessage();
+
+            assertEquals("FAILED!", withdrawal.replyTo);            
 
         }
 
